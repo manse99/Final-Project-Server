a0e23ef721005e2154101777ba7024877d8b77c1
IsaacStyle <isaac.opra@gmail.com>
2023-02-08 12:47:48 -0500

    Schemas show up in admin

--- readme/admin.py
+++ readme/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post,User,Comments,Subs
 # Register your models here.
 admin.site.register(Post)
+admin.site.register(User)
+admin.site.register(Subs)
+admin.site.register(Comments)
--- readme/models.py
+++ readme/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 class Post(models.Model):
     title = models.CharField(max_length = 100)
@@ -11,3 +12,33 @@ class Post(models.Model):
 
     def __str__(self):
         return f"{self.title}"
+
+class Subs(models.Model):
+    title = models.CharField(max_length = 20)
+    followers = ArrayField(models.IntegerField())
+    creator = models.CharField(max_length = 20)
+    post_id = ArrayField(models.IntegerField())
+    descript = models.TextField()
+
+    def __str__(self):
+        return f"{self.title}"
+
+class Comments(models.Model):
+    comment =  models.TextField(max_length = 500)
+    user_id = models.CharField(max_length = 20)
+    post_id = models.CharField(max_length = 20)
+    sub_id = models.CharField(max_length = 20)
+    up_vote = models.IntegerField()
+    down_vote = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.comment}"
+
+class User(models.Model):
+    user_name = models.CharField(max_length = 20)
+    sub_reddits = ArrayField(models.IntegerField())
+    comments = ArrayField(models.IntegerField())
+    profile_pic = models.TextField()
+
+    def __str__(self):
+        return f"{self.user_name}"
