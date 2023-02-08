0ff4156ed751f528161f770cc644908601b1c3aa
IsaacStyle <isaac.opra@gmail.com>
2023-02-08 12:22:13 -0500

    Model base

--- readme/models.py
+++ readme/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length = 100)
+    text = models.TextField()
+    img_url = models.TextField()
+    up_vote = models.IntegerField()
+    down_vote = models.IntegerField()
+    user_id = models.IntegerField()
+    sub_reddit_id = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.title}"
