bcbf55ac25fe3bbd46ab799be1660cd17a24a7d2
IsaacStyle <isaac.opra@gmail.com>
2023-02-09 09:37:52 -0500

    Serializers and Viewports create

--- readme/views.py
+++ readme/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import UserSerializer,PostSerializer,CommentsSerializer,SubsSerializer
+from .models import User,Post,Subs,Comments
+
+class PostViewSet(viewsets.ModelViewSets):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class UserViewSet(viewsets.ModelViewSets):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class SubsViewSet(viewsets.ModelViewSets):
+    queryset = Subs.objects.all()
+    serializer_class = SubsSerializer
+
+class CommentsViewSet(viewsets.ModelViewSets):
+    queryset = Comments.objects.all()
+    serializer_class = CommentsSerializer
