f93336dd033cc9ecf7f969135ebb5a96a7157a8f
IsaacStyle <isaac.opra@gmail.com>
2023-02-09 09:44:31 -0500

    basic urls

--- readme/views.py
+++ readme/views.py
@@ -3,18 +3,18 @@ from rest_framework import viewsets
 from .serializers import UserSerializer,PostSerializer,CommentsSerializer,SubsSerializer
 from .models import User,Post,Subs,Comments
 
-class PostViewSet(viewsets.ModelViewSets):
+class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-class UserViewSet(viewsets.ModelViewSets):
+class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-class SubsViewSet(viewsets.ModelViewSets):
+class SubsViewSet(viewsets.ModelViewSet):
     queryset = Subs.objects.all()
     serializer_class = SubsSerializer
 
-class CommentsViewSet(viewsets.ModelViewSets):
+class CommentsViewSet(viewsets.ModelViewSet):
     queryset = Comments.objects.all()
     serializer_class = CommentsSerializer
