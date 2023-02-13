e7b594e4b7d4f66920564874f14e9ab87c1c8ad5
IsaacStyle <isaac.opra@gmail.com>
2023-02-13 16:55:41 -0500

    Add auth

--- server/urls.py
+++ server/urls.py
@@ -18,6 +18,7 @@ from django.urls import path,include
 
 from rest_framework import routers
 from read_me.views import UserViewSet, PostViewSet, CommentsViewSet, SubsViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 
 router = routers.DefaultRouter()
 router.register(r"post", PostViewSet)
@@ -28,4 +29,6 @@ router.register(r"comments", CommentsViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name = 'token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name = 'token_refresh')    
 ]
